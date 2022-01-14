a9a4f71361b8af52fbb50ba841f0a5c842ef57b0
40D4Y5 <mohammed1alhaimi@gmail.com>
2022-01-14 18:19:04 +0800

    copy the files

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ import shutil
 
 
 class Handler(watchdog.events.PatternMatchingEventHandler):
-    def __init__(self):
+    def __init__(self, to_copy):
         # Set the patterns for PatternMatchingEventHandler
-        watchdog.events.PatternMatchingEventHandler.__init__(self, patterns=['*.csv'],
+        watchdog.events.PatternMatchingEventHandler.__init__(self, patterns=['*'],
                                                              ignore_directories=True, case_sensitive=False)
+        self.to_copy = to_copy
 
     def on_created(self, event):
         print("Watchdog received created event - % s." % event.src_path)
@@ -17,7 +18,32 @@ class Handler(watchdog.events.PatternMatchingEventHandler):
 
     def on_modified(self, event):
         print("Watchdog received modified event - % s." % event.src_path)
-        # Event is modified, you can process it now
+        # Source path
+        source = event.src_path
+
+        # Destination path
+        destination = self.to_copy + src_path.split(r"\\")[-1]
+        print(destination)
+
+        # Copy the content of
+        # source to destination
+
+        try:
+            shutil.copy(source, destination)
+            print("File copied successfully.")
+
+        # If source and destination are same
+        except shutil.SameFileError:
+            print("Source and destination represents the same file.")
+
+        # If there is any permission issue
+        except PermissionError:
+            print("Permission denied.")
+
+        # For other errors
+        except:
+            print("Error occurred while copying file.")
+            # Event is modified, you can process it now
 
 
 if __name__ == "__main__":
@@ -25,7 +51,7 @@ if __name__ == "__main__":
     src_path = input("Enter folder path Please: ")
     copy_to = input("Enter Folder to copy the files to :")
 
-    event_handler = Handler()
+    event_handler = Handler(copy_to)
     observer = watchdog.observers.Observer()
     observer.schedule(event_handler, path=src_path, recursive=True)
     observer.start()
